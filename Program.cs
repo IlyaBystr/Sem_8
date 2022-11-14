0f2a20f195b2fec4db75777e52d5d32a93046640
Ilya <fox367213@mail.com>
2022-11-14 12:01:18 +0300

    5 задача

--- Program.cs
+++ Program.cs
@@ -213,9 +213,9 @@ int arraySizeZ = 2;
 int minNumber = 1;
 int maxNumber = 99;
 int[,,] Array = new int[arraySizeX, arraySizeY, arraySizeZ];
-FillArrayRandomNumber(Array, minNumber, maxNumber);
+GetArray(Array, minNumber, maxNumber);
 PrintArray(Array);
-void FillArrayRandomNumber(int[,,] array, int minNumber = 0, int maxNumber = 9)
+void GetArray(int[,,] array, int minNumber = 0, int maxNumber = 9)
 {
     Random rnd = new Random();
 
@@ -275,3 +275,47 @@ void PrintArray(int[,,] array)
     }
 }
 */
+
+/*
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+*/
+
+int len = 4;
+int[,] array = new int[len, len];
+GetArray(array, len);
+PrintArray(array);
+
+
+
+void GetArray(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int h = 0; h < n * n; h++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
