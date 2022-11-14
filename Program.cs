e2d68c52b0b9b28cf965c65f9775ee92f86c2dc4
Ilya <fox367213@mail.com>
2022-11-14 10:39:49 +0300

    2 задача

--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
 ﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 Например, задан массив:
 */
-
+/*
 Console.WriteLine("Введите количество строк");
 int m = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите количество столбцов");
 int n = int.Parse(Console.ReadLine()!);
-Random rnd = new Random();
 int[,] array = GetArray(m, n);
 Printarray(array);
 SortArray(array);
-Console.WriteLine( );
+Console.WriteLine();
 Printarray(array);
 
 int[,] GetArray(int m, int n)
@@ -58,4 +57,66 @@ void SortArray(int[,] array)
             }
         }
     }
+}
+*/
+
+/*
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+*/
+
+Console.WriteLine("Введите количество строк");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов");
+int n = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(m, n);
+Printarray(array);
+MinSum(array);
+
+int[,] GetArray(int m, int n)
+{
+
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int L = 0; L < n; L++)
+        {
+            arr[i, L] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+void Printarray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void MinSum(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.WriteLine($"номер строки с наименьшей суммой элементов:{minSumRow + 1} строка");
 }
